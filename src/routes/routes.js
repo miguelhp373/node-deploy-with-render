f11d9d5c292b73fb06c28a9353dc68ccd59de257
Miguel Henrique <miguelhp373@gmail.com>
2023-05-29 21:58:34 -0300

    fix - cors sintax errors

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,25 @@
 const { Router } = require("express");
 const { userscontroller } = require("../controllers/users.controller");
+const cors = require('cors');
 
 const userRoutes = Router();
 
-userRoutes.use(corsMiddleware); // Aplica o middleware cors
+const corsMiddleware = cors({
+  origin: '*', // Define a origem permitida (todos os domínios)
+  credentials: true, // Permite o uso de cookies e cabeçalhos de autorização com HTTPS
+  allowedHeaders: 'Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale', // Define os cabeçalhos permitidos
+  methods: 'GET,PUT,POST,PATCH,DELETE,OPTIONS' // Define os métodos permitidos
+});
+
+userRoutes.use(corsMiddleware);
 
 userRoutes.use(
-    '/users',
-    userRoutes.get('/all',userscontroller.getAllUsers),
-    userRoutes.get('/:id',userscontroller.getUserById),
-    userRoutes.post('/',userscontroller.putNewUser),
-    userRoutes.patch('/:id',userscontroller.patchUser),
-    userRoutes.delete('/:id',userscontroller.deleteUser),
-)
+  '/users',
+  userRoutes.get('/all',userscontroller.getAllUsers),
+  userRoutes.get('/:id',userscontroller.getUserById),
+  userRoutes.post('/',userscontroller.putNewUser),
+  userRoutes.patch('/:id',userscontroller.patchUser),
+  userRoutes.delete('/:id',userscontroller.deleteUser),
+);
 
 module.exports = { userRoutes };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const express = require("express")
-const { userRoutes } = require("./routes/routes")
+const express = require("express");
+const { userRoutes } = require("./routes/routes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRoutes);
 
-app.listen(port, ()=> console.log(`HTTP Server Running on Port: ${port}`))
+app.listen(port, () => console.log(`HTTP Server Running on Port: ${port}`));
